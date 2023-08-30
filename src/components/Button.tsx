cc01753e03519c8b669e2077d5aacfa3d0fd2550
Kertsu <kurtddbigtas@gmail.com>
2023-08-30 17:21:49 +0800

    edited button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,17 @@
 const Button = ({ setToggleAddTask, toggleAddTask, tasks }: any) => {
   return (
     <>
-      <button
-        disabled={tasks.length == 0 ? true : false}
-        onClick={() => setToggleAddTask((prevToggle: any) => !prevToggle)}
-        className={`rounded-md text-lg text-white px-4 py-2 disabled:cursor-not-allowed ${
-          toggleAddTask ? "bg-red-500" : "bg-green-500"
-        }`}
-      >
-        {toggleAddTask ? "Close" : "Add"}
-      </button>
+      {!(tasks.length == 0) && (
+        <button
+          disabled={tasks.length == 0 ? true : false}
+          onClick={() => setToggleAddTask((prevToggle: any) => !prevToggle)}
+          className={`rounded-md text-lg text-white px-4 py-2 disabled:cursor-not-allowed ${
+            toggleAddTask ? "bg-red-500" : "bg-green-500"
+          }`}
+        >
+          {toggleAddTask ? "Close" : "Add"}
+        </button>
+      )}
     </>
   );
 };
